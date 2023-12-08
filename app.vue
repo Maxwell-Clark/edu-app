c2d731ed2e2c7d11d2f0d7c95422df1f3f1ff700
Max <maxwell6636@gmail.com>
2023-12-08 16:45:12 -0600

    init commit

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <Home />
+  </div>
+</template>
+<script setup lang="ts">
+import Home from "~/pages/Home.vue";
+</script>
---
+++ pages/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="leading-normal tracking-normal text-white gradient">
+    <Header />
+    <Hero />
+    <Teasers />
+    <call-to-action />
+    <Footer />
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+  Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+.gradient {
+  /* background: linear-gradient(90deg, #6200ee 0%, #03dac5 100%);  */
+  /*previous colors #d53369 #daae51 */
+  background: linear-gradient(90deg, #212529 0%, #4e148c 100%);
+}
+</style>
